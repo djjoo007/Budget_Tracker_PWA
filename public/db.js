93527e9dc76bb391dcf88fdcde46399337796c72
Joo Heredia <djjoo007uw@gmail.com>
2020-10-24 21:53:34 -0700

    update db.js

--- public/db.js
+++ public/db.js
@@ -10,7 +10,7 @@ request.onupgradeneeded = function(event) {
 request.onsuccess = function(event) {
     db = event.target.result;
     if (navigator.onLine) {
-        checkDatabase();
+        checkDB();
     };
 };
 
@@ -20,5 +20,32 @@ request.onerror = function(event) {
 
 function saveRecord(record) {
     const transaction = db.transaction(['pending'], 'readwrite');
-    
-}
+    const store = transaction.objectStore('pending');
+    store.add(record);
+};
+
+function checkDB() {
+    const transaction = db.transaction(['pending'], 'readwrite');
+    const store = transaction.objectStore('pending');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(['pending'], 'readwrite');
+                    store.clear();
+                });
+        }
+    };
+};
+
+window.addEventListener('online', checkDatabase);
